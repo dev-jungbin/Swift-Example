133d674cb51ee22708045a6967c9e3e1edda29c3
dev-madilyn <lollin0@naver.com>
2022-05-05 22:38:24 +0900

    Create: Model

---
+++ MVVM-Example/MVVM-Example/New Group/Model.swift
@@ -0,0 +1,16 @@
+//
+//  Model.swift
+//  MVVM-Example
+//
+//  Created by madilyn on 2022/05/05.
+//
+
+import Foundation
+
+struct ModelElement {
+    var id: Int
+    var userID: Int
+    var title: String
+}
+
+typealias Model = [ModelElement]
